00745b372973563db27b5699c54fbe32e8aa31da
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2024-01-02 10:22:34 +0100

    modification job02 et ajout job03 terminé

--- jour01/job03.py
+++ jour01/job03.py
@@ -2,8 +2,12 @@ class operation:
     def __init__(self, nombre1, nombre2):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
+    
+    def addition(self):
+        return self.nombre1 + self.nombre2
 
 exemple = operation(10, 12)
 
 print(f" le nombre 1 est {exemple.nombre1}")
 print(f" le nombre 2 est {exemple.nombre2}")
+print(f" nombre1 + nombre2 = {exemple.addition()}")
