8c12d253ed727d81e6a834a3af8edfd9c5cbbeaa
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2024-01-02 12:08:20 +0100

    job08 terminé

---
+++ jour01/job08.py
@@ -0,0 +1,26 @@
+class Cercle :
+    def __init__(self, rayon):
+        self.rayon = rayon
+
+    def changerRayon(self, rayon):
+        self.rayon = rayon
+
+    def aire(self):
+        return self.rayon**2*3.14 
+
+    def circonference(self):
+        return self.rayon*2*3.14 
+    
+    def diametre(self):
+        return self.rayon*2
+    
+    def afficherInfos(self):
+        print(f"le rayon est {self.rayon}")
+        print(f"l'aire est {self.aire()}")
+        print(f"la circonference est {self.circonference()}")
+        print(f"le diametre est {self.diametre()}")
+
+cercle1 = Cercle(5)
+cercle1.afficherInfos()
+cercle1.changerRayon(10)
+cercle1.afficherInfos()
