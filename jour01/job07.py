ccf11969971a09557e8129414b959813e834533f
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2024-01-02 11:50:20 +0100

    job07 terminé

---
+++ jour01/job07.py
@@ -0,0 +1,26 @@
+class Personnage : 
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def haut(self):
+        self.y += 1
+
+    def bas(self):
+        self.y -= 1
+    
+    def droite(self):
+        self.x += 1
+
+    def gauche(self):
+        self.x -= 1
+
+    def position(self):
+        return(f"le personnage se trouve en {self.x} et {self.y}")
+    
+
+perso = Personnage()
+print(perso.position())
+perso.haut()
+print(perso.position())
+        
