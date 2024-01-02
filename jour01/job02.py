cf39283075272207908a1be70eadd5257125ef2b
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2024-01-02 10:14:44 +0100

    ajout job01 et job02

---
+++ jour01/job02.py
@@ -0,0 +1,7 @@
+class operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+print(f" le nombre 1 est {operation(10 ,12).nombre1}")
+print(f" le nombre 2 est {operation(10, 12).nombre2}")
